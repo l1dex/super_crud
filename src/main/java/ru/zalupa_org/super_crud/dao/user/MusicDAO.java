8b41fc1fbfd888d5ced36d05f82b7d74c1bfa831
l1dex <l1dex@github.com>
2020-09-29 13:07:15 +0300

    MusicDAO fixed

--- src/main/java/ru/zalupa_org/super_crud/dao/user/MusicDAO.java
+++ src/main/java/ru/zalupa_org/super_crud/dao/user/MusicDAO.java
@@ -1,8 +1,13 @@
 package ru.zalupa_org.super_crud.dao.user;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 import ru.zalupa_org.super_crud.model.Music;
 
+import javax.transaction.Transactional;
+
+@Repository
+@Transactional
 public interface MusicDAO extends CrudRepository<Music,Long> {
 
 }
