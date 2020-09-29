6e6365e41ff3cc0a54d560df1f2cb668ee61d769
l1dex <l1dex@github.com>
2020-09-29 13:06:40 +0300

    dao interfaces added

---
+++ src/main/java/ru/zalupa_org/super_crud/dao/user/MusicDAO.java
@@ -0,0 +1,8 @@
+package ru.zalupa_org.super_crud.dao.user;
+
+import org.springframework.data.repository.CrudRepository;
+import ru.zalupa_org.super_crud.model.Music;
+
+public interface MusicDAO extends CrudRepository<Music,Long> {
+
+}
