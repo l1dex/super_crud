7833f0af9d27b8fb9d8830914f2f087cb6e1a3a7
l1dex <l1dex@github.com>
2020-10-14 14:58:17 +0300

    Aspect Monitored with annotation

---
+++ src/main/java/ru/zalupa_org/super_crud/aspect/Monitoring.java
@@ -0,0 +1,11 @@
+package ru.zalupa_org.super_crud.aspect;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Monitoring {
+}
